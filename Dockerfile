a510120317df40448bbd8afee4f7d0a4c7bd7482
Matthew Klein <matthew-a-klein@github.com>
2023-10-17 11:03:30 +0100

    Usable front page

--- Dockerfile
+++ Dockerfile
@@ -3,12 +3,12 @@ ENV NODE_ENV development
 # Add a work directory
 WORKDIR /app
 # Cache and Install dependencies
-COPY package.json .
-COPY package-lock.json .
+COPY package*.json ./
+
 RUN npm install
 # Copy app files
 COPY . .
 # Expose port
 EXPOSE 3000
 # Start the app
-CMD [ "npm", "start" ]
+CMD ["npm", "start"]
--- src/Home.tsx
+++ src/Home.tsx
@@ -1,18 +1,23 @@
-import React from "react";
-import ProfilePicture from "./images/profile_picture.jpg";
-
 const Home = () => {
   return (
-    <div className="h-screen w-screen bg-[url('./images/background.jpg')] absolute text-white font-mono text-3xl">
-      <div className="flex">
-        <div>
-          <div className=" ">Hello there! I'm Alex!</div>
-          <div>I'm a software developer</div>
-          <div>
-            I'm currently a student at computer science at King's College,
-            London.
-          </div>
-          <div>Check out my portfolio on github.</div>
+    <div className="h-screen w-screen bg-[url('./images/background.jpg')] absolute text-white font-mono">
+      <div className="text-center lg:text-left">
+        <div className="text-3xl mt-16 lg:text-5xl lg:ml-32 lg:mt-48 ">
+          Hello there! I'm Matthew!
+        </div>
+        <div className=" text-3xl mt-2 lg:mt-6 lg:text-5xl lg:ml-32">
+          I'm a software developer
+        </div>
+        <div className="text-xl mt-16 lg:ml-32">
+          I'm currently a studying computer science at King's College, London.
+        </div>
+        <div className="text-xl lg:ml-32">
+          Check out my portfolio on{" "}
+          <a href="https://github.com/matthew-a-klein">github</a>.
+        </div>
+        <div className="text-xl mt-8 lg:ml-32">
+          I am interested in web and development, compilers and machine
+          learning.
         </div>
       </div>
     </div>
