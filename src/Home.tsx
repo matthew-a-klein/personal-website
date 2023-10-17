bd094fbf1a425175b7b809909bd1810293de83bd
Matthew Klein <matthew-a-klein@github.com>
2023-10-17 11:29:11 +0100

    Minor changes

--- src/Home.tsx
+++ src/Home.tsx
@@ -6,7 +6,7 @@ const Home = () => {
           Hello there! I'm Matthew!
         </div>
         <div className=" text-3xl mt-2 lg:mt-6 lg:text-5xl lg:ml-32">
-          I'm a software developer
+          I'm a software developer.
         </div>
         <div className="text-xl mt-16 lg:ml-32">
           I'm currently a studying computer science at King's College, London.
@@ -16,7 +16,7 @@ const Home = () => {
           <a href="https://github.com/matthew-a-klein">github</a>.
         </div>
         <div className="text-xl mt-8 lg:ml-32">
-          I am interested in web and development, compilers and machine
+          I am interested in web and app development, compilers and machine
           learning.
         </div>
       </div>
