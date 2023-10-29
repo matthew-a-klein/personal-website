ba1da3cd06747aa2de1de176da884dd6d9cc15f8
Matthew Klein <matthew-a-klein@github.com>
2023-10-29 10:34:17 +0000

    Make not found screen full height

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -4,15 +4,17 @@ import { Link } from "react-router-dom";
 const NotFound = () => {
   return (
     <Base>
-      <div className="text-2xl lg:text-4xl text-center mt-8 lg:mt-36">
-        Hmmmm, doesn't look like that page exists.
-      </div>
-      <div className="text-lg lg:text-2xl lg:mt-4 text-center">
-        Click{" "}
-        <Link to="" className="underline lg:underline-offset-4">
-          here
-        </Link>{" "}
-        to go home.
+      <div className="h-screen">
+        <div className="text-2xl lg:text-4xl text-center mt-20 lg:mt-36">
+          Hmmmm, doesn't look like that page exists.
+        </div>
+        <div className="text-lg lg:text-2xl lg:mt-4 text-center">
+          Click{" "}
+          <Link to="" className="underline lg:underline-offset-4">
+            here
+          </Link>{" "}
+          to go home.
+        </div>
       </div>
     </Base>
   );
