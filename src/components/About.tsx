da3d07ef56a38d13c32f8fbb6684c13f00d05df0
Matthew Klein <matthew-a-klein@github.com>
2023-12-15 14:58:12 +0100

    Add sefaria profile

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -13,9 +13,18 @@ const About = ({ arrowClickHandler }: AboutInterface) => {
         My name is Matthew Alexander Klein, I am a software engineer, currently
         studying for a Bsc in Computer Science at King's College, London.
       </div>
-      <div className=" mx-5 text-xl mt-16 lg:ml-32">
+      <div className="mx-5 text-xl mt-16 lg:ml-32">
         I grew up in London and spent four years in Yeshivot in Israel.
       </div>
+      <div className="mx-5 text-xl mt-4 lg:ml-32">I sometimes give talks on Halachah and halachic history. Here's a{' '}
+      <a
+            className="underline underline-offset-4"
+            href="https://www.sefaria.org/profile/matthew-alexander-klein"
+            target="_blank"
+          >
+            link
+          </a>
+         {' '}to my source sheets.</div>
       <div className=" mx-5 text-xl mt-16 lg:ml-32">
         {" "}
         I have experience in full-stack web and app development, compilers, and
